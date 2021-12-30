d7074a76b20a4f8807a573f964a57e107071545c
vladthetree <50account!>
2021-12-30 22:36:15 +0100

    save

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,14 @@
+import DB.Mongo.MongoConnection;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class main {
 
   public static void main(String[] args) {
+
+    String path = "C://Users//vlady//Documents//TestFiles//A//TextCSV.csv";
+    MongoCollection<Document> col = MongoConnection.getCollectionFromDatabase(
+        "uat", "bsonObject");
 
   }
 
